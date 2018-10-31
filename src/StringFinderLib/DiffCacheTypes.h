e250232f106b3251c15fb5aa3b323c31451eb3e7
ruksov <krasochenko.yegor@apriorit.com>
2018-10-31 10:03:36 +0200

    Chache hash function for diff cache container

--- src/StringFinderLib/DiffCacheTypes.h
+++ src/StringFinderLib/DiffCacheTypes.h
@@ -30,8 +30,7 @@ namespace sf::lib
     {
         size_t operator()(DiffCacheKey const& k) const noexcept
         {
-            const size_t h1 = std::hash<char>()(k.DiffByte);
-            return h1 ^ (k.DiffOffset << 1);
+            return k.DiffByte ^ (k.DiffOffset << 8);
         }
     };
 
